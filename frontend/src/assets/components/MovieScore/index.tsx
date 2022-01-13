4651eaf24947feec3c85798d54fe7ec774765b67
Nilvan Calista <nil.calista@gmail.com>
2022-01-13 20:07:15 -0300

    MovieCard

---
+++ frontend/src/assets/components/MovieScore/index.tsx
@@ -0,0 +1,19 @@
+import MovieStars from '../MovieStars';
+import './style.css';
+
+function MovieScore() {
+
+    const score = 5.8;
+    const count = 76;
+
+    return (
+        <div className="dsmovie-score-container">
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+            <MovieStars />
+            <p className="dsmovie-score-count">{count} avaliações</p>
+        </div>
+    );
+
+}
+
+export default MovieScore;
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,37 @@
+import MovieCard from "assets/components/MovieCard";
 import Pagination from "assets/components/Pagination";
 
 function Listing() {
 
     return (
-        <Pagination />
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
